d25e80dd2cd9660d60ec60bd601a27b69ef7953f
Alper Akca <alperakca79@outlook.com>
2022-11-25 23:31:52 +0300

    Add project files.

---
+++ dotTempCleanerConsoleEdition/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+
+namespace dotTempCleanerConsoleEdition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Coded By AlperAkca79
+
+            bool keepUse = true;
+
+            // Actions     
+            while (keepUse)
+            {
+                Console.WriteLine("Type 'w' for About .TempCleanerConsoleEdition");
+                Console.WriteLine("Type 'x' for Delete 'Temp' Folder");
+                Console.WriteLine("Type 'y' for Delete 'Prefetch' Folder");
+                Console.WriteLine("Type 'z' for Delete 'Temp' and 'Prefetch' Folders");
+
+                Console.Write("What do you want to do? (w/x/y/z): ");
+                string selectedNumber = Convert.ToString(Console.ReadLine());
+
+                // Action - W
+                if (selectedNumber == "w")
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine("----------------------------------------------About--------------------------------------------------------------------");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(".TempCleanerConsoleEdition Version: 2.0.0");
+                    Console.WriteLine(".NET version: 6.0.0");
+                    Console.WriteLine("License: MIT License");
+                    Console.WriteLine(" ");
+                    Console.WriteLine("----------------------------------------------Links--------------------------------------------------------------------");
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Website: " + "https://alperakca79.github.io/dotTempCleanerConsoleEdition");
+                    Console.WriteLine("Source Code: " + "https://github.com/AlperAkca79/dotTempCleanerConsoleEdition");
+                    Console.WriteLine("Found Bug? Report Issue on GitHub: " + "https://github.com/AlperAkca79/dotTempCleanerConsoleEdition/issues/new");
+                    Console.WriteLine(" ");
+                    Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Coded By AlperAkca79");
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Press ENTER for Continue");
+                    Console.WriteLine(" ");
+                }
+
+                // Action - X
+                else if (selectedNumber == "x")
+                {
+                    var tempPath = new DirectoryInfo(@"C:\Windows\Temp");
+                    tempPath.Attributes = tempPath.Attributes & ~FileAttributes.ReadOnly;
+                    Console.WriteLine("Deleting temp folder");
+                    tempPath.Delete(true);
+                    Console.Write("Selected Folders Cleaned!");
+                }
+
+                // Action - Y
+                else if (selectedNumber == "y")
+                {
+                    var prefetchPath = new DirectoryInfo(@"C:\Windows\Prefetch");
+                    prefetchPath.Attributes = prefetchPath.Attributes & ~FileAttributes.ReadOnly;
+                    Console.WriteLine("Deleting prefetch folder");
+                    prefetchPath.Delete(true);
+                    Console.Write("Selected Folders Cleaned!");
+                }
+
+                // Action - Z
+                else if (selectedNumber == "z")
+                {
+                    // Temp
+                    var tempPath = new DirectoryInfo(@"C:\Users\Alper\Desktop\temp");
+                    tempPath.Attributes = tempPath.Attributes & ~FileAttributes.ReadOnly;
+                    Console.WriteLine("Deleting temp folder");
+                    tempPath.Delete(true);
+                    Console.Write("Selected Folders Cleaned!");
+
+                    // Prefetch
+                    var prefetchPath = new DirectoryInfo(@"C:\Windows\Prefetch");
+                    prefetchPath.Attributes = prefetchPath.Attributes & ~FileAttributes.ReadOnly;
+                    Console.WriteLine("Deleting prefetch folder");
+                    prefetchPath.Delete(true);
+                    Console.Write("Selected Folders Cleaned!");
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
